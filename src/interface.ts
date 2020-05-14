bf58846de84e5b8f6eca4e84e036f9c9a137120c
Noah B Johnson <noah@heartfelt.tech>
2020-05-14 16:16:23 -0500

    constructor params

--- src/interface.ts
+++ src/interface.ts
@@ -1,17 +1,43 @@
-import { IsAlpha, IsHash, IsIP, IsNotEmpty } from 'class-validator'
+import { IsAlpha, IsBoolean, IsHash, IsIP, IsNotEmpty, IsNumber, IsOptional, Max, Min } from 'class-validator'
 
 export class NamecheapParams {
   @IsAlpha()
   @IsNotEmpty()
-  apiUser: string
+  user: string
 
   @IsNotEmpty()
   @IsHash('md5')
-  apiKey: string
+  key: string
 
   @IsNotEmpty()
   @IsIP(4)
-  clientIp: string
+  ip: string
+}
 
+export class NamecheapOptions {
+  @IsOptional()
+  @IsBoolean()
   useSandbox?: boolean
+
+  @IsOptional()
+  @IsBoolean()
+  debug?: boolean
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Max(100)
+  maximumRetryAttempts?: number
+
+  @IsOptional()
+  @IsNumber()
+  @Min(.1)
+  @Max(60)
+  retryDelay?: number
+}
+
+// http://developer.namecheap.com/docs/doku.php?id=overview:2.environments
+export enum Endpoints {
+  SANDBOX = 'https://api.sandbox.namecheap.com/xml.response',
+  PRODUCTION = 'https://api.namecheap.com/xml.response'
 }
--- src/namecheap.ts
+++ src/namecheap.ts
@@ -1,15 +1,33 @@
-import { NamecheapParams } from './interface'
+import { Endpoints, NamecheapOptions, NamecheapParams } from './interface'
+import { validateSync } from 'class-validator'
 
+/**
+ * To use:
+ # 1) browse to http://www.sandbox.namecheap.com/
+ # 2) create account, my account > manage profile > api access > enable API
+ # 3) add your IP address to whitelist
+ */
 export class Namecheap {
   private readonly apiUsername: string
   private readonly apiKey: string
   private readonly clientIp: string
-  private readonly endpointUrl: string
+  private readonly endpointUrl: string = Endpoints.PRODUCTION
+  private readonly debug: boolean = false
+  private readonly maximumRetryAttempts: number = 1
+  private readonly retryDelay: number = .1
 
-  constructor (params: NamecheapParams) {
-    this.apiUsername = params.apiUser
-    this.apiKey = params.apiKey
-    this.clientIp = params.clientIp
-    this.endpointUrl = `https://api.${params.useSandbox ? 'sandbox.' : ''}namecheap.com/xml.response`
+  constructor (params: NamecheapParams, options: NamecheapOptions) {
+    validateSync(params)
+    this.apiUsername = params.user
+    this.apiKey = params.key
+    this.clientIp = params.ip
+
+    if (options) {
+      if (!!options.useSandbox) this.endpointUrl = Endpoints.SANDBOX
+      this.debug = !!options.debug
+      if (options.maximumRetryAttempts) this.maximumRetryAttempts = options.maximumRetryAttempts
+      if (options.retryDelay) this.retryDelay = options.retryDelay
+    }
   }
+
 }
